95ec5dd840ee8a9d0f66256d0df7c506e317fa20
brianhankins <brian.a.hankins@gmail.com>
2017-05-12 12:39:08 -0500

    Repository Page

--- app.js
+++ app.js
@@ -12,6 +12,10 @@
                 templateUrl: 'user.html',
                 controller: 'UserController'
             })
+            .when('/repo/:username/:reponame', {
+                templateUrl: 'repo.html',
+                controller: 'RepoController'
+            })
             .otherwise({
                 redirectTo: '/main'
             });
